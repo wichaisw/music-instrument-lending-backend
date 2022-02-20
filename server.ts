51bdc5b21b4078a2cd3d1c9f1986ed0665287d34
wichaisw <wichai.sw19@gmail.com>
2022-02-20 16:45:12 +0700

    fix: endpoints & cors

--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
 import instrumentRoutes from './routes/instruments';
 import imageRoutes from './routes/images';
 
@@ -7,8 +9,12 @@ const app: Application = express();
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 
-app.use('/admin/products', instrumentRoutes);
-app.use('/admin/images', imageRoutes);
+app.use(cors({
+  origin: `${process.env.ORIGIN}`
+}));
+
+app.use('/products', instrumentRoutes);
+app.use('/images', imageRoutes);
 
 app.listen(8000, () => {
   console.log("Express server is running on port 8000")
